1461b5fdab9c6379bcc9f50813c587ba6ed348ef
patrickcooper95 <patrickcooper1995@gmail.com>
2020-06-20 00:53:16 -0500

    062020 - Fixed logging errors in daemon

--- wapid.py
+++ wapid.py
@@ -9,8 +9,9 @@ import wapi.led_utils as utils
 LOGGER = logging.getLogger()
 logging.basicConfig(filename='daemon.log', level=logging.INFO,
                     format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
-
-logging.INFO("Daemon starting.")
+fh = logging.FileHandler("./daemon.log")
+LOGGER.addHandler(fh)
+logging.info("Daemon starting.")
 
 
 class LedWorker(threading.Thread):
@@ -53,18 +54,20 @@ def main_program():
         # dynamic = cur.execute(f'SELECT * FROM colors WHERE name={new_state}').fetchall()[0][4]
         conn.close()
         if not current_state == new_state:
-            logging.INFO("Change detected. Updating lights to %s.", new_state)
+            logging.info("Change detected. Updating lights to %s.", new_state)
             utils.loop = False
 
             led_worker = LedWorker(target=set_program, args=(new_state,))
             utils.loop = True
 
-            logging.INFO("Starting new thread.")
+            logging.info("Starting new thread.")
             led_worker.start()
             current_state = new_state
 
 
-context = daemon.DaemonContext()
+context = daemon.DaemonContext(
+    files_preserve = [fh.stream,]
+    )
 
 with context:
     main_program()
