1ccf4de2c5c3b93dc2a7cd09a2c6f203c59c16f3
Patrick <patrickcooper1995@gmail.com>
2020-06-19 23:59:52 -0500

    061920 - Absolute paths updated

--- wapi/configs.py
+++ wapi/configs.py
@@ -2,7 +2,7 @@ import yaml
 
 
 # Load in config object
-with open("config.yaml", 'r') as stream:
+with open("/home/pi/WAPI/smarty-lamps/config.yaml", 'r') as stream:
     try:
         config = yaml.safe_load(stream)
     except yaml.YAMLError as exc:
