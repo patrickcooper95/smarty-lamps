6a5fcdb204d56d35d941c637d2aeae6ea59b5029
Patrick <patrickcooper1995@gmail.com>
2020-09-02 23:25:41 -0400

    090220 - Fix config path

--- wapi/configs.py
+++ wapi/configs.py
@@ -4,11 +4,11 @@ import yaml
 
 # Get environment variable for environment - default to PROD if not found
 environment = os.getenv("ENV", "PROD")
-base_path = os.path.join("home", "pi", environment, "smarty-lamps")
+base_path = os.path.join("/home", "pi", environment, "smarty-lamps")
 
 
 # Load in config object
-with open(os.path.join(base_path, "wapi", "config.yaml", 'r')) as stream:
+with open(os.path.join(base_path, "wapi", "config.yaml"), 'r') as stream:
     try:
         config = yaml.safe_load(stream)
     except yaml.YAMLError as exc:
