78759f14453865b24ce686aedcfb4cc207582c61
Patrick <patrickcooper1995@gmail.com>
2021-03-22 20:49:07 -0400

    032021 - fix console

--- programs/console.py
+++ programs/console.py
@@ -6,17 +6,17 @@ def console(light_obj, np):
     """Create a starship blinking console effect."""
     init_color = (255, 100, 0)
 
-    np.fill((0, 0, 0))
-    init_num = random.randint(0, np.num)
+    np.fill(init_color)
+    init_num = random.randint(0, light_obj.num)
     init_pixels = []
     for p in range(init_num):
-        pixel = random.randint(0, np.num)
+        pixel = random.randint(0, light_obj.num)
         init_pixels.append(pixel)
         np[pixel] = init_color
 
     while light_obj.loop:
         try:
-            pixel = random.randint(0, np.num)
+            pixel = random.randint(0, light_obj.num)
             index = random.randint(1, 3)
             sleep = [0.5, 1, 1.5]
             np[pixel] = (0, 0, 0)
