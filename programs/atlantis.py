5f0f7a80099889165f7f399249e5e0b235589c03
patrickcooper95 <patrickcooper1995@gmail.com>
2021-04-07 21:38:16 -0400

    040721 - update to atlantis program

--- programs/atlantis.py
+++ programs/atlantis.py
@@ -9,25 +9,29 @@ def atlantis(light_obj, np):
             for count in range(light_obj.num):
                 if not light_obj.loop:
                     break
-                red = 255
-                for x in range(255):
-                    np[count] = ((red, 20, 147))
-                    red -= 1
+
                 np[count] = ((0, 128, 128))
-                time.sleep(1)
+                time.sleep(0.1)
+
+#            time.sleep(2.0)
 
             for count in range(light_obj.num):
                 if not light_obj.loop:
                     break
 
                 np[count] = ((0, 0, 255))
-                time.sleep(1)
+                time.sleep(0.1)
+
+#            time.sleep(2.0)
+
             for count in range(light_obj.num):
                 if not light_obj.loop:
                     break
 
                 np[count] = ((255, 20, 147))
-                time.sleep(1)
+                time.sleep(0.1)
+
+#            time.sleep(2.0)
 
         except KeyboardInterrupt as e:
             raise (e)
