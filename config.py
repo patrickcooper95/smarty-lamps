145ad1e9c4eb6044429a1c5508d680ec1572e7ad
Patrick <patrickcooper1995@gmail.com>
2021-06-01 21:10:41 -0400

    060121 - start using the yaml

--- config.py
+++ config.py
@@ -1,8 +1,16 @@
 import logging
 
+import yaml
+
+with open("./wapi/config.yaml", 'r') as config_file:
+    try:
+        base_configs = yaml.safe_load(config_file)
+    except yaml.YAMLError as exc:
+        print(exc)
+
 
 def logging_config(name):
-    logging.basicConfig(filename='/home/pi/logs/smarty-lamps.log',
+    logging.basicConfig(filename=f'{base_configs["CONFIG_LOG_DIR"]}smarty-lamps.log',
                         level=logging.INFO,
                         format='%(asctime)s | %(levelname)s | %(name)s | %(threadName)s | %(message)s')
     return logging.getLogger(name)
